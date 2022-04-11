6348ae4d0f056f78cb97f7e25cd2e88e61566a2a
clxyify <98536033+clxyify@users.noreply.github.com>
2022-04-11 12:40:32 -0400

    Create SynX.lua

---
+++ SynX.lua
@@ -0,0 +1,15 @@
+--paste this in your init script otherwise it won't work!
+
+print("Synapse X Script Support By SpeedSterKawaii")--watermark :)
+
+local function syn()
+  wait(0.01)
+end
+
+local function PROTOSMASHER_LOADED()
+  if not syn then print("passed on protosmasher") else warn("not synapsex") end
+end
+
+local function is_synapse_function()
+  --confirm
+end
