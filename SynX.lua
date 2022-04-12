d770896f6f48c05591c776003e846102a753e449
clxyify <98536033+clxyify@users.noreply.github.com>
2022-04-12 10:10:46 -0400

    Update SynX.lua

--- SynX.lua
+++ SynX.lua
@@ -3,7 +3,7 @@
 print("Synapse X Script Support By SpeedSterKawaii")--watermark :)
 
 local function syn()
-  wait(0.01)
+  wait(0.01)--TODO: prob return to make it more real
 end
 
 local function PROTOSMASHER_LOADED()
@@ -13,3 +13,6 @@ end
 local function is_synapse_function()
   --confirm
 end
+
+--test it out (dont need to use but still)
+if is_synapse_function then print("exists!") else warn("uh-oh! doesnt exist!") end
