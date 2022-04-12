39b37ce6fc7cd894819c0131ba84ce822ae11315
clxyify <98536033+clxyify@users.noreply.github.com>
2022-04-12 10:18:02 -0400

    Update SynX.lua
    updaated

--- SynX.lua
+++ SynX.lua
@@ -1,18 +1,26 @@
 --paste this in your init script otherwise it won't work!
+--DO NOT EXECUTE ON SYNAPSE X OR KRNL!
 
-print("Synapse X Script Support By SpeedSterKawaii")--watermark :)
+print("Synapse X Script Support By SpeedSterKawaii")--watermark (remove if you skid)
+if is_synapse_function then print("Synapse X Support Already Exists!") print("Cant register functions!") wait(15000) else print("Starting up Synapse X Functions") end
+
+wait(3)--do cooldown so the function detect doesnt break
 
 local function syn()
   wait(0.01)--TODO: prob return to make it more real
 end
 
-local function PROTOSMASHER_LOADED()
+local function PROTOSMASHER_LOADED()--you dont need this function but makes it more epic
   if not syn then print("passed on protosmasher") else warn("not synapsex") end
 end
 
 local function is_synapse_function()
-  --confirm
+  wait(0.01)--didnt use return cuz we got nothing to return (except function)
 end
 
 --test it out (dont need to use but still)
 if is_synapse_function then print("exists!") else warn("uh-oh! doesnt exist!") end
+
+--confirm it started and other creds and stuff
+print("Synapse X Functions Registered!")
+print("You can add Synapse X Functions as well -> https://github.com/clxyify/SynX-Script-Support")
